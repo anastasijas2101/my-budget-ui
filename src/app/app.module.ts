644d33de79930dcfee00086ad6caa9041396f3ee
anastasijas2101 <anastasijas101@gmail.com>
2024-08-16 17:38:02 +0200

    Add login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: "", component: AppComponent},
+      {path: "login", component: LoginComponent},
+      {path: "account", component: AccountComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
